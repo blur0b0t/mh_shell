65764763cda8576bbfd3af65dc4324433d757efe
blur0b0t <jatin.gcp@gmail.com>
2023-09-29 04:27:39 -0700

    mod:t5 tokenizer modified

--- ft_model_pp/ft_mod1.sh
+++ ft_model_pp/ft_mod1.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+
+# sbatch -x idc-beta-batch-pvc-node-[03,20,21] --priority 0 --job-name fts1 ft_mod1.sh
+export batch_script="ft_mod1.sh"
+# -----------set new job dep--------------
+echo "got current job name=$SLURM_JOB_NAME"
+export cji=$(echo -n $SLURM_JOB_NAME | tail -c 1)
+export nji=$(( cji + 1 ))
+# export nji=$($SLURM_JOB_NAME | tail -c 1| awk '{print $1 + 1}')
+export njname="fts$nji"
+echo "new job name=$njname"
+export njid=$(sbatch -x idc-beta-batch-pvc-node-[03,20,21] --priority 0 --job-name $njname --begin=now+60 --dependency=afterany:$SLURM_JOB_ID $batch_script | sed -n 's/.*job //p')
+echo "new job created with id: $njid"
+# -------------------end------------------
+
+
+
 echo "starting fine tuning model"
 cd "/home/u131168/mh_shell/ft_models/intel-extension-for-transformers/workflows/chatbot/fine_tuning"
 pip install -r "requirements.txt"
@@ -6,9 +23,11 @@ cd "/home/u131168/mh_shell/ft_models/intel-extension-for-transformers/workflows/
 # pip install intel-extension-for-transformers
 # pip install fschat==0.1.2
 pip install git+https://github.com/huggingface/transformers
+pip install tokenizers
 
 
-# sbatch -x idc-beta-batch-pvc-node-[03,20] --job-name mhsft1 --priority=0 ft_mod1.sh
+
+# sbatch -x idc-beta-batch-pvc-node-[03,20,21] --job-name mhsft1 --priority=0 ft_mod1.sh
 # sbatch -w idc-beta-batch-pvc-node-[05] --job-name mhsft1 --priority=0 ft_mod1.sh
 
 
@@ -16,18 +35,18 @@ export train_file="/home/u131168/mh_shell/data/f_traind.csv"
 
 export model_path="google/flan-t5-xl"
 # export checkpoint_path="/home/u131168/mh_shell/ft_models/flan-t5-xl_peft_finetuned_model/checkpoint-36000"
-export checkpoint_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_peft_finetuned_model/"
+export checkpoint_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_mt5/"
 export checkpoint_name=$(ls $checkpoint_dir | grep checkpoint | tail -1)
 export checkpoint_path="$checkpoint_dir$checkpoint_name"
 echo $checkpoint_path
 
-export output_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_peft_finetuned_model"
+export output_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_mt5"
 
+        # --resume_from_checkpoint $checkpoint_path \
 
 
 python finetune_seq2seq.py \
         --model_name_or_path $model_path \
-        --resume_from_checkpoint $checkpoint_path \
         --bf16 True \
         --train_file $train_file \
         --per_device_train_batch_size 2 \
