bfc20dad21f8ac9bcd5f5ab3b751b7d451df5371
blur0b0t <jatin.gcp@gmail.com>
2023-10-09 04:12:37 -0700

    mod:1api_env

--- ft_model_pp/ft_mod1.sh
+++ ft_model_pp/ft_mod1.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 #!/bin/sh
 
 
 # sbatch -x idc-beta-batch-pvc-node-[03,09,20,21] --priority 0 --job-name fts1 ft_mod1.sh
-# sbatch -x idc-beta-batch-pvc-node-[03,09,20,21] --priority 0 --job-name fts2 --dependency=afterany:27250 ft_mod1.sh
+# sbatch -x idc-beta-batch-pvc-node-[03,09,20,21] --priority 0 --job-name fts3 --dependency=afterany:28437 ft_mod1.sh
 
 
 
@@ -14,9 +15,29 @@ export nji=$(( cji + 1 ))
 # export nji=$($SLURM_JOB_NAME | tail -c 1| awk '{print $1 + 1}')
 export njname="fts$nji"
 echo "new job name=$njname"
-export njid=$(sbatch -x idc-beta-batch-pvc-node-[03,09,20,21] --priority 0 --job-name $njname --begin=now+60 --dependency=afterany:$SLURM_JOB_ID $batch_script | sed -n 's/.*job //p')
+export njid=$(sbatch -x idc-beta-batch-pvc-node-[03,09,14,20,21] --priority 0 --job-name $njname --begin=now+60 --dependency=afterany:$SLURM_JOB_ID $batch_script | sed -n 's/.*job //p')
 echo "new job created with id: $njid"
 # -------------------end------------------
+
+
+
+
+echo "----------checking if gpu available on current job-----------------"
+# oneapi env and checking gpu
+echo "-------------------------------------------"
+groups
+source /opt/intel/oneapi/setvars.sh --force
+sycl-ls
+export num_gpu="$(sycl-ls |grep "GPU" |wc -l)"
+echo "num_gpu=$num_gpu\n"
+export num_cpu="$(sycl-ls |grep "Xeon" |wc -l)"
+echo "num_cpu=$num_cpu\n"
+if [ $num_gpu == 0 && $num_cpu == 1] 
+then 
+    echo "---GPU not available exiting--------"
+    scancel $SLURM_JOB_ID
+fi 
+echo "-------------------------------------------"
 
 
 
