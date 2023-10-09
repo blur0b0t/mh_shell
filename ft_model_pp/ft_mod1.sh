db5196acd24e8b5e1c121a367faffa7457d7fe82
blur0b0t <jatin.gcp@gmail.com>
2023-10-09 00:18:15 -0700

    ft_model_v3

--- ft_model_pp/ft_mod1.sh
+++ ft_model_pp/ft_mod1.sh
@@ -41,7 +41,7 @@ export model_path="google/flan-t5-xl"
 # export model_path="/home/u131168/mh_shell/ft_models/flan-t5-xl_mt5_v1"
 
 # export checkpoint_path="/home/u131168/mh_shell/ft_models/flan-t5-xl_peft_finetuned_model/checkpoint-36000"
-export checkpoint_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_mt5_v2/"
+export checkpoint_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_mt5_v3/"
 export checkpoint_name=$(ls $checkpoint_dir | grep checkpoint | tail -1)
 export checkpoint_path="$checkpoint_dir$checkpoint_name"
 echo $checkpoint_path
