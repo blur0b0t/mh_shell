62cbe1fc6b395fa7d48104d166fb4d32f8e495d3
blur0b0t <jatin.gcp@gmail.com>
2023-10-17 00:13:59 -0700

    mod:cp

--- p_custom_pp/p_custom.py
+++ p_custom_pp/p_custom.py
@@ -12,7 +12,7 @@ mh_dir='mh_shell'
 # start_index=[0,9500,19000]
 # end_index=[9500,19000,29000]
 # file_name=['0_10k.csv','10_20k.csv','20_30k.csv']
-pred_file_name="full_pred7.csv"
+pred_file_name="full_pred8.csv"
 pred_file_path=f"/home/u131168/{mh_dir}/data/custom_pred/{pred_file_name}"
 # start_index=subprocess.get_output("tail {} -n 1 | awk -F' ' '{print $1}'".format(file_name))
 start_index=subprocess.check_output("tail "+pred_file_path+" -n 1 | awk -F' ' '{print $1}'",shell=True)
@@ -47,7 +47,10 @@ from tokenizers import AddedToken
 from transformers import T5Tokenizer
 
 # checkpoint_dir="/home/u131168/mh_shell/ft_models/flan-t5-xl_peft_finetuned_model/"
-checkpoint_dir=f"/home/u131168/{mh_dir}/ft_models/flan-t5-xl_mt5_v4/"
+
+# checkpoint_dir=f"/home/u131168/{mh_dir}/ft_models/flan-t5-xl_mt5_v4/"  #----------xl
+checkpoint_dir=f"/home/u131168/{mh_dir}/ft_models/flan-t5-xxl_mt5_v1/"   #------xxl
+
 checkpoint_name=subprocess.check_output(f"ls {checkpoint_dir} | grep checkpoint | tail -1",shell=True)
 checkpoint_name=str(checkpoint_name).replace("b'","").replace("\\n'","")
 checkpoint_path=checkpoint_dir+checkpoint_name
@@ -59,7 +62,9 @@ model = AutoPeftModelForSeq2SeqLM.from_pretrained(model_path, )
 
 # tokenizer:
 # tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xl")
-tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xl")
+
+# tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xl") #---------------xl
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xxl") #-------xxl
 
 # stokens_v1=["`","~","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]","|","\\",":",";","\"","'","<",">","?","/","\n","\t"," "]
 # stokens_v2=["{","}","<","<<"]
